df72cd5ed3302f48cdcb4785e69ec2b7c1f284f4
Artem Falcon <artem.falcon@gmail.com>
2012-11-15 15:29:47 +0400

    Most significant changes: typedefize structs, unions, enums. Correct hardware structures. Firmware init is now broken, sigh :(

--- osx-sasmegaraid/SASMegaRAID/Hardware.h
+++ osx-sasmegaraid/SASMegaRAID/Hardware.h
@@ -62,21 +62,25 @@ typedef enum {
 	MRAID_STAT_OK =                 0x00,
 } mraid_status_t;
 
+typedef struct {
+    UInt8                           mse_data[MRAID_SENSE_SIZE];
+} __attribute__((packed)) mraid_sense;
+
 /* Scatter gather access elements */
-struct mraid_sg32 {
+typedef struct {
     UInt32                          addr;
     UInt32                          len;
-} __attribute__((packed));
-struct mraid_sg64 {
+} __attribute__((packed)) mraid_sg32;
+typedef struct {
     UInt64                          addr;
     UInt32                          len;
-} __attribute__((packed));
-union mraid_sgl {
-    struct mraid_sg32               sg32[1];
-    struct mraid_sg64               sg64[1];
-} __attribute__((packed));
+} __attribute__((packed)) mraid_sg64;
+typedef union {
+    mraid_sg32               sg32[1];
+    mraid_sg64               sg64[1];
+} __attribute__((packed)) mraid_sgl;
 
-struct mraid_frame_header {
+typedef struct {
     UInt8                           mrh_cmd;
     UInt8                           mrh_sense_len;
     UInt8                           mrh_cmd_status;
@@ -85,94 +89,90 @@ struct mraid_frame_header {
     UInt8                           mrh_lun_id;
     UInt8                           mrh_cdb_len;
     UInt8                           mrh_sg_count;
-    UInt8                           mrh_context;
-    UInt8                           mrh_pad0;
-    UInt8                           mrh_flags;
-    UInt8                           mrh_timeout;
-    UInt8                           mrh_data_len;
-} __attribute__((packed));
-struct mraid_init_frame {
-    struct mraid_frame_header       mif_header;
+    UInt32                          mrh_context;
+    UInt32                          mrh_pad0;
+    UInt16                          mrh_flags;
+    UInt16                          mrh_timeout;
+    UInt32                          mrh_data_len;
+} __attribute__((packed)) mraid_frame_header;
+typedef struct {
+    mraid_frame_header       mif_header;
     UInt64                          mif_qinfo_new_addr;
     UInt64                          mif_qinfo_old_addr;
     UInt32                          mif_reserved[6];
-} __attribute__((packed));
+} __attribute__((packed)) mraid_init_frame;
 /* Queue init */
-struct mraid_init_qinfo {
+typedef struct {
     UInt32                          miq_flags;
     UInt32                          miq_rq_entries;
 	UInt64                          miq_rq_addr;
 	UInt64                          miq_pi_addr;
 	UInt64                          miq_ci_addr;
-} __attribute__((packed));
-struct mraid_io_frame {
-    struct mraid_frame_header       mif_header;
-    UInt32                          mif_sense_addr_lo;
-    UInt32                          mif_sense_addr_hi;
-    UInt32                          mif_lba_lo;
-    UInt32                          mif_lba_hi;
-    union mraid_sgl                 mif_sgl;
-} __attribute__((packed));
-struct mraid_pass_frame {
-    struct mraid_frame_header       mpf_header;
-    UInt32                          mpf_sense_addr_lo;
-    UInt32                          mpf_sense_addr_hi;
+} __attribute__((packed)) mraid_init_qinfo;
+typedef struct {
+    mraid_frame_header       mif_header;
+    UInt64                          mif_sense_addr;
+    UInt64                          mif_lba;
+    mraid_sgl                 mif_sgl;
+} __attribute__((packed)) mraid_io_frame;
+typedef struct {
+    mraid_frame_header       mpf_header;
+    UInt64                          mpf_sense_addr;
     UInt8                           mpf_cdb[16];
-    union mraid_sgl                 mpf_sgl;
-} __attribute__((packed));
+    mraid_sgl                 mpf_sgl;
+} __attribute__((packed)) mraid_pass_frame;
 #define MRAID_DCMD_FRAME_SIZE	40
-struct mraid_dcmd_frame {
-    struct mraid_frame_header       mdf_header;
+typedef struct {
+    mraid_frame_header       mdf_header;
     UInt32                          mdf_opcode;
     UInt8                           mdf_mbox[MRAID_MBOX_SIZE];
-    union mraid_sgl                 mdf_sgl;
-} __attribute__((packed));
-struct mraid_abort_frame {
-    struct mraid_frame_header       maf_header;
+    mraid_sgl                 mdf_sgl;
+} __attribute__((packed)) mraid_dcmd_frame;
+typedef struct {
+    mraid_frame_header       maf_header;
     UInt32                          maf_abort_context;
     UInt32                          maf_pad;
-    UInt32                          maf_abort_mraid_addr_lo;
-    UInt32                          maf_abort_mraid_addr_hi;
+    UInt64                          maf_abort_mraid_addr;
     UInt32                          maf_reserved[6];
-} __attribute__((packed));
-struct mraid_smp_frame {
-    struct mraid_frame_header       msf_header;
+} __attribute__((packed)) mraid_abort_frame;
+typedef struct {
+    mraid_frame_header       msf_header;
     UInt64                          msf_sas_addr;
     union {
-        struct mraid_sg32           sg32[2];
-        struct mraid_sg64           sg64[2];
+        mraid_sg32           sg32[2];
+        mraid_sg64           sg64[2];
     } msf_sgl;
-} __attribute__((packed));
-struct mraid_stp_frame {
-    struct mraid_frame_header       msf_header;
+} __attribute__((packed)) mraid_smp_frame;
+typedef struct {
+    mraid_frame_header       msf_header;
     UInt16                          msf_fis[10];
     UInt32                          msf_stp_flags;
     union {
-        struct mraid_sg32           sg32[2];
-        struct mraid_sg64           sg64[2];
+        mraid_sg32           sg32[2];
+        mraid_sg64           sg64[2];
     } msf_sgl;
-} __attribute__((packed));
+} __attribute__((packed)) mraid_stp_frame;
 
-union mraid_frame {
-    struct mraid_frame_header       mrr_header;
-    struct mraid_init_frame         mrr_init;
-    struct mraid_io_frame           mrr_io;
-    struct mraid_pass_frame         mrr_pass;
-    struct mraid_dcmd_frame         mrr_dcmd;
-    struct mraid_abort_frame        mrr_abort;
-    struct mraid_smp_frame          mrr_smp;
-    struct mraid_stp_frame          mrr_stp;
+typedef union {
+    mraid_frame_header       mrr_header;
+    mraid_init_frame                mrr_init;
+    mraid_io_frame           mrr_io;
+    mraid_pass_frame         mrr_pass;
+    mraid_dcmd_frame         mrr_dcmd;
+    mraid_abort_frame        mrr_abort;
+    mraid_smp_frame          mrr_smp;
+    mraid_stp_frame          mrr_stp;
     UInt8                           mrr_bytes[MRAID_FRAME_SIZE];
-};
+} mraid_frame;
 
-struct mraid_prod_cons {
+typedef struct {
     UInt32                          mpc_producer;
     UInt32                          mpc_consumer;
     UInt32                          mpc_reply_q[1]; /* Compensate for 1 extra reply per spec */
-};
+} mraid_prod_cons;
 
 /* mraid_ctrl_info */
-struct mraid_ctrl_props {
+typedef struct {
 	UInt16		mcp_seq_num;
 	UInt16		mcp_pred_fail_poll_interval;
 	UInt16		mcp_intr_throttle_cnt;
@@ -195,50 +195,50 @@ struct mraid_ctrl_props {
 	UInt8			mcp_restore_hotspare_on_insertion;
 	UInt8			mcp_expose_encl_devices;
 	UInt8			mcp_reserved[38];
-} __attribute__((packed));
-struct mraid_info_pci {
+} __attribute__((packed)) mraid_ctrl_props;
+typedef struct {
 	UInt16		mip_vendor;
 	UInt16		mip_device;
 	UInt16		mip_subvendor;
 	UInt16		mip_subdevice;
-	UInt16		mip_reserved[24];
-} __attribute__((packed));
+	UInt8		mip_reserved[24];
+} __attribute__((packed)) mraid_info_pci;
 /* Host interface info */
-struct mraid_info_host {
+typedef struct {
 	UInt8		mih_type;
 	UInt8		mih_reserved[6];
 	UInt8		mih_port_count;
 	UInt64      mih_port_addr[8];
-} __attribute__((packed));
+} __attribute__((packed)) mraid_info_host;
 /* Device interface info */
-struct mraid_info_device {
+typedef struct {
 	UInt8		mid_type;
 	UInt8		mid_reserved[6];
 	UInt8		mid_port_count;
-	UInt8		mid_port_addr[8];
-} __attribute__((packed));
+	UInt64		mid_port_addr[8];
+} __attribute__((packed)) mraid_info_device;
 /* Firmware component info */
-struct mraid_info_component {
+typedef struct {
 	char			mic_name[8];
 	char			mic_version[32];
 	char			mic_build_date[16];
 	char			mic_build_time[16];
-} __attribute__((packed));
+} __attribute__((packed)) mraid_info_component;
 
 /* MRAID_DCMD_CTRL_GETINFO */
-struct mraid_ctrl_info {
-	struct mraid_info_pci       mci_pci;
-	struct mraid_info_host      mci_host;
-	struct mraid_info_device	mci_device;
+typedef struct {
+	mraid_info_pci       mci_pci;
+	mraid_info_host      mci_host;
+	mraid_info_device	mci_device;
     
 	/* Firmware components that are present and active. */
 	UInt32		mci_image_check_word;
 	UInt32		mci_image_component_count;
-	struct mraid_info_component mci_image_component[8];
+	mraid_info_component mci_image_component[8];
     
 	/* Firmware components that have been flashed but are inactive */
 	UInt32		mci_pending_image_component_count;
-	struct mraid_info_component mci_pending_image_component[8];
+	mraid_info_component mci_pending_image_component[8];
     
 	UInt8			mci_max_arms;
 	UInt8			mci_max_spans;
@@ -285,31 +285,24 @@ struct mraid_ctrl_info {
     
 	UInt8			mci_ecc_bucket_count;
 	UInt8			mci_reserved2[11];
-	struct mraid_ctrl_props	mci_properties;
+	mraid_ctrl_props	mci_properties;
 	char			mci_package_version[0x60];
 	UInt8			mci_pad[0x800 - 0x6a0];
-} __attribute__((packed));
+} __attribute__((packed)) mraid_ctrl_info;
 
-/* MR_DCMD_LD_GET_LIST */
-struct mraid_ld {
-	UInt8			mld_target;
-	UInt8			mld_res;
-	UInt16		mld_seq;
-} __attribute__((packed));
-
-enum mraid_iop {
+typedef enum {
     MRAID_IOP_XSCALE,
     MRAID_IOP_PPC,
     MRAID_IOP_GEN2,
     MRAID_IOP_SKINNY
-};
-struct	mraid_pci_device {
+} mraid_iop;
+typedef struct {
 	UInt16							mpd_vendor;
 	UInt16							mpd_product;
-	enum mraid_iop					mpd_iop;
-};
+	mraid_iop					mpd_iop;
+} mraid_pci_device;
 namespace mraid_structs {
-    static const struct mraid_pci_device mraid_pci_devices[] = {
+    static const mraid_pci_device mraid_pci_devices[] = {
         { PCI_VENDOR_SYMBIOS,	PCI_PRODUCT_SYMBIOS_MEGARAID_SAS,
             MRAID_IOP_XSCALE	},
         { PCI_VENDOR_SYMBIOS,	PCI_PRODUCT_SYMBIOS_MEGARAID_VERDE_ZCR,
