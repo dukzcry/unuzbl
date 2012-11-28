9e974763d5fec24413a92a49af19d388e55eb96c
Artem Falcon <artem.falcon@gmail.com>
2012-11-29 00:44:49 +0400

    Re-import struct descriptions.

--- osx-sasmegaraid/SASMegaRAID/Hardware.h
+++ osx-sasmegaraid/SASMegaRAID/Hardware.h
@@ -63,86 +63,86 @@ typedef enum {
 } mraid_status_t;
 
 typedef struct {
-    UInt8                           mse_data[MRAID_SENSE_SIZE];
+	uint8_t			mse_data[MRAID_SENSE_SIZE];
 } __attribute__((packed)) mraid_sense;
 
 /* Scatter gather access elements */
 typedef struct {
-    UInt32                          addr;
-    UInt32                          len;
+	uint32_t		addr;
+	uint32_t		len;
 } __attribute__((packed)) mraid_sg32;
 typedef struct {
-    UInt64                          addr;
-    UInt32                          len;
+	uint64_t		addr;
+	uint32_t		len;
 } __attribute__((packed)) mraid_sg64;
 typedef union {
-    mraid_sg32               sg32[1];
-    mraid_sg64               sg64[1];
+	mraid_sg32		sg32[1];
+	mraid_sg64		sg64[1];
 } __attribute__((packed)) mraid_sgl;
 
 typedef struct {
-    UInt8                           mrh_cmd;
-    UInt8                           mrh_sense_len;
-    UInt8                           mrh_cmd_status;
-    UInt8                           mrh_scsi_status;
-    UInt8                           mrh_target_id;
-    UInt8                           mrh_lun_id;
-    UInt8                           mrh_cdb_len;
-    UInt8                           mrh_sg_count;
-    UInt32 mrh_context;
-    /* XXX: unsuccessfull */
-    UInt32 mrh_pad0;
-    //UInt8 mrh_pad0;
-    UInt16 mrh_flags;
-    //UInt8 mrh_flags;
-    UInt16 mrh_timeout;
-    //UInt8 mrh_timeout;
-    UInt32 mrh_data_len;
-    //UInt8 mrh_data_len;
+	uint8_t			mrh_cmd;
+	uint8_t			mrh_sense_len;
+	uint8_t			mrh_cmd_status;
+	uint8_t			mrh_scsi_status;
+	uint8_t			mrh_target_id;
+	uint8_t			mrh_lun_id;
+	uint8_t			mrh_cdb_len;
+	uint8_t			mrh_sg_count;
+	uint32_t		mrh_context;
+    /* XXX: broken */
+	/*uint32_t		mrh_pad0;
+	uint16_t		mrh_flags;
+	uint16_t		mrh_timeout;
+	uint32_t		mrh_data_len;*/
+	uint8_t		mrh_pad0;
+	uint8_t		mrh_flags;
+	uint8_t		mrh_timeout;
+	uint8_t		mrh_data_len;
 } __attribute__((packed)) mraid_frame_header;
 typedef struct {
-    mraid_frame_header       mif_header;
-    UInt64                          mif_qinfo_new_addr;
-    UInt64                          mif_qinfo_old_addr;
-    UInt32                          mif_reserved[6];
+	mraid_frame_header	mif_header;
+	uint64_t		mif_qinfo_new_addr;
+	uint64_t		mif_qinfo_old_addr;
+	uint32_t		mif_reserved[6];
 } __attribute__((packed)) mraid_init_frame;
 /* Queue init */
 typedef struct {
-    UInt32                          miq_flags;
-    UInt32                          miq_rq_entries;
-	UInt64                          miq_rq_addr;
-	UInt64                          miq_pi_addr;
-	UInt64                          miq_ci_addr;
+	uint32_t		miq_flags;
+	uint32_t		miq_rq_entries;
+	uint64_t		miq_rq_addr;
+	uint64_t		miq_pi_addr;
+	uint64_t		miq_ci_addr;
 } __attribute__((packed)) mraid_init_qinfo;
 typedef struct {
     mraid_frame_header       mif_header;
-    UInt64                          mif_sense_addr;
-    UInt64                          mif_lba;
+	uint64_t		mif_sense_addr;
+	uint64_t		mif_lba;
     mraid_sgl                 mif_sgl;
 } __attribute__((packed)) mraid_io_frame;
 typedef struct {
     mraid_frame_header       mpf_header;
-    UInt64                          mpf_sense_addr;
-    UInt8                           mpf_cdb[16];
+	uint64_t		mpf_sense_addr;
+	uint8_t			mpf_cdb[16];
     mraid_sgl                 mpf_sgl;
 } __attribute__((packed)) mraid_pass_frame;
 #define MRAID_DCMD_FRAME_SIZE	40
 typedef struct {
-    mraid_frame_header       mdf_header;
-    UInt32                          mdf_opcode;
-    UInt8                           mdf_mbox[MRAID_MBOX_SIZE];
-    mraid_sgl                 mdf_sgl;
+	mraid_frame_header mdf_header;
+	uint32_t		mdf_opcode;
+	uint8_t			mdf_mbox[MRAID_DCMD_FRAME_SIZE];
+	mraid_sgl		mdf_sgl;
 } __attribute__((packed)) mraid_dcmd_frame;
 typedef struct {
     mraid_frame_header       maf_header;
-    UInt32                          maf_abort_context;
-    UInt32                          maf_pad;
-    UInt64                          maf_abort_mraid_addr;
-    UInt32                          maf_reserved[6];
+	uint32_t		maf_abort_context;
+	uint32_t		maf_pad;
+	uint64_t		maf_abort_mfi_addr;
+	uint32_t		maf_reserved[6];
 } __attribute__((packed)) mraid_abort_frame;
 typedef struct {
     mraid_frame_header       msf_header;
-    UInt64                          msf_sas_addr;
+    uint64_t		msf_sas_addr;
     union {
         mraid_sg32           sg32[2];
         mraid_sg64           sg64[2];
@@ -150,8 +150,8 @@ typedef struct {
 } __attribute__((packed)) mraid_smp_frame;
 typedef struct {
     mraid_frame_header       msf_header;
-    UInt16                          msf_fis[10];
-    UInt32                          msf_stp_flags;
+	uint16_t		msf_fis[10];
+	uint32_t		msf_stp_flags;
     union {
         mraid_sg32           sg32[2];
         mraid_sg64           sg64[2];
@@ -159,68 +159,68 @@ typedef struct {
 } __attribute__((packed)) mraid_stp_frame;
 
 typedef union {
-    mraid_frame_header       mrr_header;
-    mraid_init_frame                mrr_init;
-    mraid_io_frame           mrr_io;
-    mraid_pass_frame         mrr_pass;
-    mraid_dcmd_frame         mrr_dcmd;
-    mraid_abort_frame        mrr_abort;
-    mraid_smp_frame          mrr_smp;
-    mraid_stp_frame          mrr_stp;
-    UInt8                           mrr_bytes[MRAID_FRAME_SIZE];
+	mraid_frame_header mrr_header;
+	mraid_init_frame	mrr_init;
+	mraid_io_frame	mrr_io;
+	mraid_pass_frame	mrr_pass;
+	mraid_dcmd_frame	mrr_dcmd;
+	mraid_abort_frame	mrr_abort;
+	mraid_smp_frame	mrr_smp;
+	mraid_stp_frame	mrr_stp;
+	uint8_t			mrr_bytes[MRAID_FRAME_SIZE];
 } mraid_frame;
 
 typedef struct {
-    UInt32                          mpc_producer;
-    UInt32                          mpc_consumer;
-    UInt32                          mpc_reply_q[1]; /* Compensate for 1 extra reply per spec */
+	uint32_t		mpc_producer;
+	uint32_t		mpc_consumer;
+	uint32_t		mpc_reply_q[1]; /* Compensate for 1 extra reply per spec */
 } mraid_prod_cons;
 
 /* mraid_ctrl_info */
 typedef struct {
-	UInt16		mcp_seq_num;
-	UInt16		mcp_pred_fail_poll_interval;
-	UInt16		mcp_intr_throttle_cnt;
-	UInt16		mcp_intr_throttle_timeout;
-	UInt8			mcp_rebuild_rate;
-	UInt8			mcp_patrol_read_rate;
-	UInt8			mcp_bgi_rate;
-	UInt8			mcp_cc_rate;
-	UInt8			mcp_recon_rate;
-	UInt8			mcp_cache_flush_interval;
-	UInt8			mcp_spinup_drv_cnt;
-	UInt8			mcp_spinup_delay;
-	UInt8			mcp_cluster_enable;
-	UInt8			mcp_coercion_mode;
-	UInt8			mcp_alarm_enable;
-	UInt8			mcp_disable_auto_rebuild;
-	UInt8			mcp_disable_battery_warn;
-	UInt8			mcp_ecc_bucket_size;
-	UInt16		mcp_ecc_bucket_leak_rate;
-	UInt8			mcp_restore_hotspare_on_insertion;
-	UInt8			mcp_expose_encl_devices;
-	UInt8			mcp_reserved[38];
+	uint16_t		mcp_seq_num;
+	uint16_t		mcp_pred_fail_poll_interval;
+	uint16_t		mcp_intr_throttle_cnt;
+	uint16_t		mcp_intr_throttle_timeout;
+	uint8_t			mcp_rebuild_rate;
+	uint8_t			mcp_patrol_read_rate;
+	uint8_t			mcp_bgi_rate;
+	uint8_t			mcp_cc_rate;
+	uint8_t			mcp_recon_rate;
+	uint8_t			mcp_cache_flush_interval;
+	uint8_t			mcp_spinup_drv_cnt;
+	uint8_t			mcp_spinup_delay;
+	uint8_t			mcp_cluster_enable;
+	uint8_t			mcp_coercion_mode;
+	uint8_t			mcp_alarm_enable;
+	uint8_t			mcp_disable_auto_rebuild;
+	uint8_t			mcp_disable_battery_warn;
+	uint8_t			mcp_ecc_bucket_size;
+	uint16_t		mcp_ecc_bucket_leak_rate;
+	uint8_t			mcp_restore_hotspare_on_insertion;
+	uint8_t			mcp_expose_encl_devices;
+	uint8_t			mcp_reserved[38];
 } __attribute__((packed)) mraid_ctrl_props;
 typedef struct {
-	UInt16		mip_vendor;
-	UInt16		mip_device;
-	UInt16		mip_subvendor;
-	UInt16		mip_subdevice;
-    UInt8 mip_reserved[24];
+	uint16_t		mip_vendor;
+	uint16_t		mip_device;
+	uint16_t		mip_subvendor;
+	uint16_t		mip_subdevice;
+	uint8_t			mip_reserved[24];
 } __attribute__((packed)) mraid_info_pci;
 /* Host interface info */
 typedef struct {
-	UInt8		mih_type;
-	UInt8		mih_reserved[6];
-	UInt8		mih_port_count;
-	UInt64      mih_port_addr[8];
+	uint8_t			mih_type;
+	uint8_t			mih_reserved[6];
+	uint8_t			mih_port_count;
+	uint64_t		mih_port_addr[8];
 } __attribute__((packed)) mraid_info_host;
 /* Device interface info */
 typedef struct {
-	UInt8		mid_type;
-	UInt8		mid_reserved[6];
-	UInt8		mid_port_count;
-    UInt64 mid_port_addr[8];
+	uint8_t			mid_type;
+	uint8_t			mid_reserved[6];
+	uint8_t			mid_port_count;
+	uint64_t		mid_port_addr[8];
 } __attribute__((packed)) mraid_info_device;
 /* Firmware component info */
 typedef struct {
@@ -237,62 +237,62 @@ typedef struct {
 	mraid_info_device	mci_device;
     
 	/* Firmware components that are present and active. */
-	UInt32		mci_image_check_word;
-	UInt32		mci_image_component_count;
+	uint32_t		mci_image_check_word;
+	uint32_t		mci_image_component_count;
 	mraid_info_component mci_image_component[8];
     
 	/* Firmware components that have been flashed but are inactive */
-	UInt32		mci_pending_image_component_count;
+	uint32_t		mci_pending_image_component_count;
 	mraid_info_component mci_pending_image_component[8];
     
-	UInt8			mci_max_arms;
-	UInt8			mci_max_spans;
-	UInt8			mci_max_arrays;
-	UInt8			mci_max_lds;
+	uint8_t			mci_max_arms;
+	uint8_t			mci_max_spans;
+	uint8_t			mci_max_arrays;
+	uint8_t			mci_max_lds;
 	char			mci_product_name[80];
 	char			mci_serial_number[32];
-	UInt32		mci_hw_present;
-	UInt32		mci_current_fw_time;
-	UInt16		mci_max_cmds;
-	UInt16		mci_max_sg_elements;
-	UInt32		mci_max_request_size;
-	UInt16		mci_lds_present;
-	UInt16		mci_lds_degraded;
-	UInt16		mci_lds_offline;
-	UInt16		mci_pd_present;
-	UInt16		mci_pd_disks_present;
-	UInt16		mci_pd_disks_pred_failure;
-	UInt16		mci_pd_disks_failed;
-	UInt16		mci_nvram_size;
-	UInt16		mci_memory_size;
-	UInt16		mci_flash_size;
-	UInt16		mci_ram_correctable_errors;
-	UInt16		mci_ram_uncorrectable_errors;
-	UInt8			mci_cluster_allowed;
-	UInt8			mci_cluster_active;
-	UInt16		mci_max_strips_per_io;
+	uint32_t		mci_hw_present;
+	uint32_t		mci_current_fw_time;
+	uint16_t		mci_max_cmds;
+	uint16_t		mci_max_sg_elements;
+	uint32_t		mci_max_request_size;
+	uint16_t		mci_lds_present;
+	uint16_t		mci_lds_degraded;
+	uint16_t		mci_lds_offline;
+	uint16_t		mci_pd_present;
+	uint16_t		mci_pd_disks_present;
+	uint16_t		mci_pd_disks_pred_failure;
+	uint16_t		mci_pd_disks_failed;
+	uint16_t		mci_nvram_size;
+	uint16_t		mci_memory_size;
+	uint16_t		mci_flash_size;
+	uint16_t		mci_ram_correctable_errors;
+	uint16_t		mci_ram_uncorrectable_errors;
+	uint8_t			mci_cluster_allowed;
+	uint8_t			mci_cluster_active;
+	uint16_t		mci_max_strips_per_io;
     
-	UInt32		mci_raid_levels;
+	uint32_t		mci_raid_levels;
     
-	UInt32		mci_adapter_ops;
+	uint32_t		mci_adapter_ops;
     
-	UInt32		mci_ld_ops;
+	uint32_t		mci_ld_ops;
     
 	struct {
-		UInt8		min;
-		UInt8		max;
-		UInt8		reserved[2];
+		uint8_t		min;
+		uint8_t		max;
+		uint8_t		reserved[2];
 	} __attribute__((packed))		mci_stripe_sz_ops;
     
-	UInt32		mci_pd_ops;
+	uint32_t		mci_pd_ops;
     
-	UInt32		mci_pd_mix_support;
+	uint32_t		mci_pd_mix_support;
     
-	UInt8			mci_ecc_bucket_count;
-	UInt8			mci_reserved2[11];
+	uint8_t			mci_ecc_bucket_count;
+	uint8_t			mci_reserved2[11];
 	mraid_ctrl_props	mci_properties;
 	char			mci_package_version[0x60];
-	UInt8			mci_pad[0x800 - 0x6a0];
+	uint8_t			mci_pad[0x800 - 0x6a0];
 } __attribute__((packed)) mraid_ctrl_info;
 
 typedef enum {
