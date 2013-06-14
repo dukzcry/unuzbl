90d9b3fb5009227ea2ec0cc88546f3deae489012
A. V. Lukyanov <artem.falcon@gmail.com>
2013-06-14 16:22:44 +0400

    Move match & attach to lua side.

--- sys/modules/xgifb/xgifb.c
+++ sys/modules/xgifb/xgifb.c
@@ -2,6 +2,7 @@
 #include <sys/device.h>
 
 #include <sys/lua.h>
+#include <sys/mallocvar.h>
 #include <sys/ioctl.h>
 
 #include <dev/pci/pcivar.h>
@@ -10,8 +11,13 @@
 
 #define NAME xgifb_cd.cd_name /* files.pci's symbol */
 #define PATH "./xgifb.lua"
+MALLOC_DECLARE(M_DEVBUF);
 
 extern int luaioctl(dev_t, u_long, void *, int, struct lwp *);
+static struct {
+  klua_State *K;
+  lua_State *L;
+} xgifb_glbl;
 
 struct xgifb_softc {
   device_t sc_dev;
@@ -23,15 +29,34 @@ struct xgifb_softc {
 static int
 xgifb_match(device_t parent, cfdata_t match, void *aux)
 {
+  lua_State *L = xgifb_glbl.L;
   struct pci_attach_args *pa = (struct pci_attach_args *) aux;
 
-  return 0;
+  lua_getglobal(L, "xgifbMatch");
+  if (!lua_isfunction(L, -1))
+    return 0;
+
+  lua_pushlightuserdata(L, &parent);
+  lua_pushlightuserdata(L, &match);
+  lua_pushlightuserdata(L, pa);
+  lua_pcall(L, 3, 1, 0);
+  return lua_tointeger(L, -1);
 }
 static void
 xgifb_attach(device_t parent, device_t self, void *aux)
 {
+  lua_State *L = xgifb_glbl.L;
   struct xgifb_softc *sc = device_private(self);
   struct pci_attach_args *const pa = (struct pci_attach_args *) aux;
+
+  lua_getglobal(L, "xgifbAttach");
+  if (!lua_isfunction(L, -1))
+    return;
+
+  lua_pushlightuserdata(L, &parent);
+  lua_pushlightuserdata(L, sc);
+  lua_pushlightuserdata(L, pa);
+  lua_pcall(L, 3, 0, 0);
 }
 CFATTACH_DECL_NEW(xgifb, sizeof(struct xgifb_softc), xgifb_match,
 		  xgifb_attach, NULL, NULL);
@@ -41,32 +66,34 @@ static int
 xgifb_modcmd(modcmd_t cmd, void *opaque)
 {
   int ret = 0;
-  struct lua_create cr = {
-    .name = "xgifb",
-    .desc = "\0"
-  };
   struct lua_load l = {
     .state = "xgifb",
     .path = PATH
   };
+  klua_State *K;
 
   switch (cmd) {
   case MODULE_CMD_INIT:
-    if (luaioctl(0, LUACREATE, &cr, 0, NULL)) {
-      aprint_error("%s: LUACREATE", NAME);
+    if (!(K = klua_newstate(lua_alloc, M_DEVBUF, NAME,
+				     "XGI Volari frame buffer"))) {
+      aprint_error("%s: LUACREATE\n", NAME);
       return -1;
     }
+    xgifb_glbl.K = K; /* for new call */
+    xgifb_glbl.L = K->L;
+
     if (luaioctl(0, LUALOAD, &l, 0, NULL)) {
-      aprint_error("%s: LUALOAD", NAME);
+      aprint_error("%s: LUALOAD\n", NAME);
       goto fail;
     }
+
     ret = config_init_component(cfdriver_ioconf_xgifb,
 				    cfattach_ioconf_xgifb, cfdata_ioconf_xgifb);
     break;
   case MODULE_CMD_FINI:
-    luaioctl(0, LUADESTROY, &cr, 0, NULL);
     ret = config_fini_component(cfdriver_ioconf_xgifb,
 				  cfattach_ioconf_xgifb, cfdata_ioconf_xgifb);
+    klua_close(xgifb_glbl.K);
     break;
   default:
     ret = ENOTTY;
@@ -74,5 +101,6 @@ xgifb_modcmd(modcmd_t cmd, void *opaque)
 
   return ret;
  fail:
-  return luaioctl(0, LUADESTROY, &cr, 0, NULL);
+  klua_close(K);
+  return -1;
 }
--- sys/modules/xgifb/xgifb.lua
+++ sys/modules/xgifb/xgifb.lua
@@ -1,1 +1,13 @@
-require 'luahw'
+require 'hw'
+require 'core'
+
+function xgifbMatch(parent, match, pa)
+	 --core.print("matching\n")
+	 return 0
+end
+function xgifbAttach(parent, sc, pa)
+	 --core.print("attaching\n")
+end
+
+function onClose()
+end
