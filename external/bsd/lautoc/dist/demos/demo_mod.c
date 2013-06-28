a8a4d60994b42a1b489a72cd8eff6c75c0fed04c
A. V. Lukyanov <artem.falcon@gmail.com>
2013-06-28 16:24:33 +0400

    luaA_function_reg_void

--- external/bsd/lautoc/dist/demos/demo_mod.c
+++ external/bsd/lautoc/dist/demos/demo_mod.c
@@ -25,8 +25,8 @@ int main(int argc, char **argv) {
   lua_State* L = luaL_newstate();
   luaA_open();
   
-  luaA_function(L, add_numbers, float, 2, int, float);
-  luaA_function_void(L, hello_world, 1, char*);
+  luaA_function_reg(L, add_numbers, float, 2, int, float);
+  luaA_function_reg_void(L, hello_world, 1, char*);
   
   lua_pushcfunction(L, autocall);
   lua_setglobal(L, "autocall");
