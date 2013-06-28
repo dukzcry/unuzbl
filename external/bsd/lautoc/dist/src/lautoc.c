5b9885b07409ecf55b1e12cc1bd31e4a068b4ea2
A. V. Lukyanov <artem.falcon@gmail.com>
2013-06-28 15:08:33 +0400

    0.85 -> 0.9

--- external/bsd/lautoc/dist/src/lautoc.c
+++ external/bsd/lautoc/dist/src/lautoc.c
@@ -5,11 +5,11 @@ void luaA_open(void) {
   luaA_stack_open();
   luaA_struct_open();
   //luaA_enum_open();
-  //luaA_call_open();
+  luaA_call_open();
 }
 
 void luaA_close(void) {
-  //luaA_call_close();
+  luaA_call_close();
   //luaA_enum_close();
   luaA_struct_close();
   luaA_stack_close();
