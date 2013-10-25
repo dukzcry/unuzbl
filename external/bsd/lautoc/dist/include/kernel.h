9e238cbc244585fcb4b9910d1ba1ad3b92b7f0be
A. V. Lukyanov <artem.falcon@gmail.com>
2013-10-25 14:36:43 +0400

    Unleaky freeing.

--- external/bsd/lautoc/dist/include/kernel.h
+++ external/bsd/lautoc/dist/include/kernel.h
@@ -1,5 +1,8 @@
-#include <sys/malloc.h>
+#define _M_WAITOK	0x0000
+void *kern_malloc(unsigned long, int);
+void kern_free(void *);
+void *kern_realloc(void *, unsigned long, int);
 
-#define malloc(size) kern_malloc(size, M_WAITOK)
+#define malloc(size) kern_malloc(size, _M_WAITOK)
 #define free(ptr) kern_free(ptr)
-#define realloc(ptr, size) kern_realloc(ptr, size, M_WAITOK)
+#define realloc(ptr, size) kern_realloc(ptr, size, _M_WAITOK)
